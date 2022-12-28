f4cf7b82e85f515cabbabbe9253dccac97d84486
CemYilmaz <cemyilmaz463@gmail.com>
2022-12-28 22:13:26 +0300

    Domain Layer: Activity Repository Added

---
+++ lib/domain/activity_repository/src/activity_repository.dart
@@ -0,0 +1,25 @@
+///WeatherRepository'nin temel amacı, veri sağlayıcıyı SOYUTLAYAN bir arayüz sağlamaktır. Bu durumda, WeatherRepository, WeatherApiClient(CollectApiClient)'e bağımlı olacak ve getWeather(String city) adlı tek bir genel yöntemi kullanıma sunacaktır.
+///Yani CollectApiClientdan gelen iki methodu birleştirip tek bir methoda indirgeyeceğiz.
+
+///WeatherRepository tüketicileri, metaweather API'sine iki ağ isteğinin yapılması gerçeği gibi temel uygulama ayrıntılarına hakim değildir. WeatherRepository'nin amacı "ne"yi "nasıl"dan AYIRMAK - başka bir deyişle, belirli bir şehir için HAVA DURUMUNU NASIL ALACAĞIMIZI BİLMEMİZ GEREKİR, ancak bu verilerin nasıl veya nereden geldiğini umursamayız.
+
+import 'dart:async';
+
+import 'package:weather_frontend/data/activity_api/activity_api.dart';
+
+class ActivityRepository {
+  ActivityRepository({ActivityApiClient? activityApiClient})
+      : _activityApiClient = activityApiClient ?? ActivityApiClient();
+
+  final ActivityApiClient _activityApiClient;
+
+  Future<Activity> getActivity() async {
+    final activity = await _activityApiClient.getActivity();
+
+    return Activity(
+      id: activity.id,
+      title: activity.title,
+      description: activity.description,
+    );
+  }
+}
